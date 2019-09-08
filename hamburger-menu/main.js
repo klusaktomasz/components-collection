634f7808c29be2730397f5473f05d9ef731c5cc1
Tomasz Kłusak <klusaktomasz@gmail.com>
2019-09-08 21:50:28 +0200

    Add comments for methods

---
+++ hamburger-menu/main.js
@@ -0,0 +1,77 @@
+/**
+ * Class representing Hamburger Menu.
+ */
+class HamburgerMenu {
+  /**
+   * Create Hamburger Menu.
+   * @param {*} list — The element that is expanded by toggle. Element must have an ID.
+   * @param {*} toggle — The element that is toggle. Element must have an ID.
+   */
+  constructor (list, toggle) {
+    this.list = list;
+    this.toggle = toggle;
+    this.state = false;
+
+    this.toggleState = this.toggleState.bind(this);
+    this._showMenu = this._showMenu.bind(this);
+    this._hideMenu = this._hideMenu.bind(this);
+
+    this._setARIA();
+    this._registerEvents();
+  }
+
+  /**
+   * Set ARIA for list and toggle.
+   * @private
+   */
+  _setARIA () {
+    this.toggle.setAttribute('aria-label', 'Open menu');
+    this.toggle.setAttribute('aria-expanded', false);
+    this.toggle.setAttribute('aria-controls', this.list.id);
+
+    this.list.setAttribute('aria-hidden', true);
+    this.list.setAttribute('aria-controlledby', this.toggle.id);
+  }
+
+  /**
+   * Register necessary events for menu.
+   * @private
+   */
+  _registerEvents () {
+    this.toggle.addEventListener('click', this.toggleState);
+  }
+
+  /**
+   * Toggle menu state (hide, show).
+   * @param {boolean|null} [state] — New state for menu. If not passed, will be opposite to the current one.
+   */
+  toggleState (state = null) {
+    if (!state) {
+      state = this.state ? false : true;
+    }
+
+    if (state) this._showMenu();
+    else this._hideMenu();
+  }
+
+  /**
+   * Show menu.
+   * @private
+   */
+  _showMenu () {
+    // this.toggle.setAttribute('aria-label', 'Close menu');
+  }
+
+  /**
+   * Hide menu.
+   * @private
+   */
+  _hideMenu () {
+    //this.toggle.setAttribute('aria-label', 'Open menu');
+  }
+};
+
+new HamburgerMenu(
+  document.querySelector('#menuList'),
+  document.querySelector('#menuToggle')
+);
